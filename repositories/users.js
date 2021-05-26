156ed58e3070ce61831fc81fe398b982a386ec20
Azal Abedi <azal.abedi@gmail.com>
2021-05-26 14:16:07 -0600

    build create() method for the UsersRepo class

--- repositories/users.js
+++ repositories/users.js
@@ -15,14 +15,24 @@ class UsersRepository {
     }
 
     async getAll() {
-        return JSON.parse(await fs.promises.readFile(this.filename, { 
-            encoding: 'utf8'
-        }));
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, { 
+                encoding: 'utf8' 
+            })
+        );
+    }
+
+    async create(attrs) {
+        const records = await this.getAll();
+        records.push(attrs);
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
+
+    await repo.create({ email: 'test@test.com', password: 'password' });
 
     const users = await repo.getAll();
 
