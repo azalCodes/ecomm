176168f57436cbade9cbf52606eb3af73f7f86f0
Azal Abedi <azal.abedi@gmail.com>
2021-05-26 14:48:33 -0600

    build update() method in UserRepo class

--- repositories/users.js
+++ repositories/users.js
@@ -50,12 +50,24 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with ID:${id} not found`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
     
-    await repo.delete('fa9f9cb9');
+    await repo.update('59ac8773', { password: 'password' });
 };
 
 test();
