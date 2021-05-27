3d3ff692bcd47e3d80068035ce3dc37d6451b534
Azal Abedi <azal.abedi@gmail.com>
2021-05-27 15:12:39 -0600

    write delete functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -70,4 +70,10 @@ router.post('/admin/products/:id/edit',
     }
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+    await productsRepo.delete(req.params.id);
+
+    res.redirect('/admin/products');
+});
+
 module.exports = router;
