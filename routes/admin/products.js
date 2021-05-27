3d1783290cc44c09f614ace784f9a3913b698d92
Azal Abedi <azal.abedi@gmail.com>
2021-05-27 14:00:03 -0600

    create display and functionality of edit products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,6 +5,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -29,6 +30,38 @@ router.post(
         const image = req.file.buffer.toString('base64');
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });
+
+        res.redirect('/admin/products');
+    }
+);
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) {
+        return res.send('Product not found');
+    }
+
+    res.send(productsEditTemplate({ product }));
+});
+
+router.post('/admin/products/:id/edit', 
+    requireAuth, 
+    upload.single('iamge'),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate),
+    async (req, res) => {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {
+            await productsRepo.update(req.params.id, changes);
+        } catch (err) {
+            return res.send('Could not find item');
+        }
 
         res.redirect('/admin/products');
     }
